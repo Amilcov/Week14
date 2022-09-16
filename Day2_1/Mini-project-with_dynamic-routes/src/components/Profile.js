27719c7b5fc374f45e76d608106e481729ba3e93
Amilcov <Amilcov@yahoo.com>
2022-09-16 03:21:56 +0300

    Add files via upload

---
+++ Day2_1/Mini-project-with_dynamic-routes/src/components/Profile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink, Redirect, Route, Switch ,useParams } from 'react-router-dom';
+
+function Profile() {
+    const params = useParams();
+    const { userId } = params;
+    if(userId === "0") return <Redirect to="/"/>
+    return (
+        <div>
+          <h1> Hello from Profile {userId} !</h1>
+          <NavLink to={`/users/${userId}/posts`}> User's Posts </NavLink>
+          <NavLink to={`/users/${userId}/photos`}> User's Phots </NavLink>
+          <NavLink to={`/users/${userId}/about`}> About User </NavLink>
+
+          <Switch>
+
+            <Route to="/users/:userId/posts">
+               <h1>Posts for User {userId} </h1>
+            </Route>
+
+            <Route to="/users/:userId/photos">
+               <h1>Photos for User {userId} </h1>
+            </Route>
+            
+
+
+          </Switch>
+        </div>
+
+    )
+
+
+}
+
+export default Profile;
